8961370e2fe7447f6762c5a3c2629f49eeb9034c
Sandeep Panda <spanda@codeaurora.org>
2017-02-10 10:38:01 +0530

    clk: qcom: add MDSS PLL support for sdm630
    Add the sdm630 compatible string to MDSS PLL driver
    of_device_id table list, so that initialization of
    MDSS PLL driver takes place for sdm630 platform.
    
    Change-Id: I284ff9c07a4a971260ade399a2f7a605003ccf1d
    Signed-off-by: Sandeep Panda <spanda@codeaurora.org>

--- drivers/clk/qcom/mdss/mdss-pll.c
+++ drivers/clk/qcom/mdss/mdss-pll.c
@@ -120,7 +120,11 @@ static int mdss_pll_resource_parse(struct platform_device *pdev,
 		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
 	else if (!strcmp(compatible_stream, "qcom,mdss_dp_pll_14nm"))
 		pll_res->pll_interface_type = MDSS_DP_PLL_14NM;
-	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_sdm660")) {
+	else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_sdm630")) {
+		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
+		pll_res->target_id = MDSS_PLL_TARGET_SDM630;
+		pll_res->revision = 2;
+	} else if (!strcmp(compatible_stream, "qcom,mdss_dsi_pll_sdm660")) {
 		pll_res->pll_interface_type = MDSS_DSI_PLL_14NM;
 		pll_res->target_id = MDSS_PLL_TARGET_SDM660;
 		pll_res->revision = 2;
@@ -394,6 +398,7 @@ static int mdss_pll_remove(struct platform_device *pdev)
 static const struct of_device_id mdss_pll_dt_match[] = {
 	{.compatible = "qcom,mdss_dsi_pll_14nm"},
 	{.compatible = "qcom,mdss_dp_pll_14nm"},
+	{.compatible = "qcom,mdss_dsi_pll_sdm630"},
 	{.compatible = "qcom,mdss_dsi_pll_sdm660"},
 	{.compatible = "qcom,mdss_dp_pll_sdm660"},
 	{.compatible = "qcom,mdss_dsi_pll_12nm"},

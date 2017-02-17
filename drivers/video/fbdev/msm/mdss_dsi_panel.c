f026e511a3ae4f956a526482d087a261c2eba2f3
Francisco Franco <franciscofranco.1990@gmail.com>
2017-02-17 13:58:13 +0100

    msm: mdss: add a simple api to query the display state (on/off) at any point in time
    Change-Id: Ie6f726ad2ad0d194c46a56526181574b92884c04
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Change-Id: I5ea480e10a6b6ab5dec550819c689da3b81422e5

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -15,6 +15,7 @@
 #include <linux/pwm.h>
 #include <linux/err.h>
 #include <linux/string.h>
+#include <linux/display_state.h>
 
 #include "mdss_dsi.h"
 #include "mdss_dba_utils.h"
@@ -24,6 +25,14 @@
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
 #define VSYNC_DELAY msecs_to_jiffies(17)
+
+static bool display_on = true;
+
+bool is_display_on(void)
+{
+	return display_on;
+}
+
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
@@ -928,6 +937,8 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1013,6 +1024,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
+
+	display_on = false;
 
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,

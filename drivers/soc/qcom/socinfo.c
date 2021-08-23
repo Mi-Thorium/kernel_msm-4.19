4904df6738b5b401ea1b696c769bb95af8bc72ff
Pavel Dubrova <pashadubrova@gmail.com>
2021-08-23 17:02:24 +0300

    soc: qcom: Add soc information for SDM630
    Add SOC information like Chip ID, macros and dummy socinfo
    definitions for SDM630 SoC.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -341,6 +341,9 @@ static struct msm_soc_info cpu_of_id[] = {
 	/* SDM660 ID */
 	[317] = {MSM_CPU_SDM660, "SDM660"},
 
+	/* SDM630 ID */
+	[318] = {MSM_CPU_SDM630, "SDM630"},
+
 	/* SDM636 ID */
 	[345] = {MSM_CPU_SDM636, "SDM636"},
 
@@ -1545,6 +1548,10 @@ static void * __init setup_dummy_socinfo(void)
 	} else if (early_machine_is_sdm660()) {
 		dummy_socinfo.id = 317;
 		strlcpy(dummy_socinfo.build_id, "sdm660 - ",
+		sizeof(dummy_socinfo.build_id));
+	} else if (early_machine_is_sdm630()) {
+		dummy_socinfo.id = 318;
+		strlcpy(dummy_socinfo.build_id, "sdm630 - ",
 		sizeof(dummy_socinfo.build_id));
 	} else if (early_machine_is_sdm636()) {
 		dummy_socinfo.id = 345;

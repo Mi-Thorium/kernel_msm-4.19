c4d377fbb3a114b896b784713713a0bb52c9d29b
me-cafebabe <me.cafebabe@gmail.com>
2023-05-03 15:57:19 +0800

    techpack: stub: Define gesture_incall incase audio isn't compiled
    * Custom DT2W/S2W relies on this
    
    Change-Id: I48da0eaa91db90125cd3a7835243751f7c1944c5

--- techpack/stub/stub.c
+++ techpack/stub/stub.c
@@ -1,5 +1,13 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
+#include <linux/types.h>
+
+#if IS_ENABLED(CONFIG_TECHPACK_AUDIO_DISABLE)
+#if (defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE) || defined(CONFIG_TOUCHSCREEN_SWEEP2WAKE))
+bool gesture_incall = false;
+#endif
+#endif
+
 static void _techpack_stub(void)
 {
 }

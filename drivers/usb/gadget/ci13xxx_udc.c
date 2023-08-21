LineageOS/android_kernel_xiaomi_msm8937

usb: gadget: ci13xxx: Import Xiaomi SDM439 changes
Change-Id: Ifc5ddf03fad7587d43aa16c04cd14781b2674e73

drivers/usb/gadget/ci13xxx_udc.c
@@ -67,6 +67,9 @@
 #include <linux/usb/gadget.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/msm_hsusb.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+#include <xiaomi-sdm439/mach.h>
+#endif
 
 #include "ci13xxx_udc.h"
 
@@ -3506,12 +3509,62 @@ static int ci13xxx_vbus_draw(struct usb_gadget *_gadget, unsigned int mA)
 	return -ENOTSUPP;
 }
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+static void xiaomi_sdm439_ci13xxx_udc_connect(struct ci13xxx *udc)
+{
+	 struct usb_phy *phy = udc->transceiver;
+	 int temp;
+	 pr_err("xbt phy->flags=%d\n", phy->flags);
+
+#define MSM_USB_BASE (udc->regs)
+
+	 if (phy && (phy->flags & ENABLE_DP_MANUAL_PULLUP)) {
+		pr_err("xbt phy->flags=%d, ENABLE_DP_MANUAL_PULLUP\n", phy->flags);
+	 }
+	 usb_phy_io_write(phy,
+	 ULPI_MISC_A_VBUSVLDEXT |
+	 ULPI_MISC_A_VBUSVLDEXTSEL,
+	 ULPI_SET(ULPI_MISC_A));
+
+	 temp = readl_relaxed(USB_GENCONFIG_2);
+	 temp |= GENCONFIG_2_SESS_VLD_CTRL_EN;
+	 writel_relaxed(temp, USB_GENCONFIG_2);
+
+	 temp = readl_relaxed(USB_USBCMD);
+	 temp |= USBCMD_SESS_VLD_CTRL;
+	 writel_relaxed(temp, USB_USBCMD);
+
+	 /*
+	 * Add memory barrier as it is must to complete
+	 * above USB PHY and Link register writes before
+	 * moving ahead with USB peripheral mode enumeration,
+	 * otherwise USB peripheral mode may not work.
+	 */
+	 mb();
+#undef MSM_USB_BASE
+}
+#endif
+
 static int ci13xxx_pullup(struct usb_gadget *_gadget, int is_active)
 {
 	struct ci13xxx *udc = container_of(_gadget, struct ci13xxx, gadget);
 	unsigned long flags;
 
 	spin_lock_irqsave(udc->lock, flags);
+
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+	if (xiaomi_sdm439_mach_get()) {
+		info("%s,softconnec=%d,is_active=%d\n", __func__, udc->softconnect, is_active);
+
+		if (!udc->softconnect && is_active) {
+			info("%s %d\n", __func__, __LINE__);
+			xiaomi_sdm439_ci13xxx_udc_connect(udc);
+			mdelay(100);
+		}
+	}
+#endif
+
+
 	udc->softconnect = is_active;
 	if (((udc->udc_driver->flags & CI13XXX_PULLUP_ON_VBUS) &&
 			!udc->vbus_active) || !udc->driver) {

Mi-Thorium/kernel_msm-4.19

drivers/usb/gadget/ci13xxx_udc.c
@@ -68,6 +68,9 @@
 #include <linux/usb/gadget.h>
 #include <linux/usb/otg.h>
 #include <linux/usb/msm_hsusb.h>
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+#include <xiaomi-sdm439/mach.h>
+#endif
 
 #include "ci13xxx_udc.h"
 
@@ -3605,12 +3608,62 @@ static int ci13xxx_vbus_draw(struct usb_gadget *_gadget, unsigned int mA)
 	return -ENOTSUPP;
 }
 
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+static void xiaomi_sdm439_ci13xxx_udc_connect(struct ci13xxx *udc)
+{
+	 struct usb_phy *phy = udc->transceiver;
+	 int temp;
+	 pr_err("xbt phy->flags=%d\n", phy->flags);
+
+#define MSM_USB_BASE (udc->regs)
+
+	 if (phy && (phy->flags & ENABLE_DP_MANUAL_PULLUP)) {
+		pr_err("xbt phy->flags=%d, ENABLE_DP_MANUAL_PULLUP\n", phy->flags);
+	 }
+	 usb_phy_io_write(phy,
+	 ULPI_MISC_A_VBUSVLDEXT |
+	 ULPI_MISC_A_VBUSVLDEXTSEL,
+	 ULPI_SET(ULPI_MISC_A));
+
+	 temp = readl_relaxed(USB_GENCONFIG_2);
+	 temp |= GENCONFIG_2_SESS_VLD_CTRL_EN;
+	 writel_relaxed(temp, USB_GENCONFIG_2);
+
+	 temp = readl_relaxed(USB_USBCMD);
+	 temp |= USBCMD_SESS_VLD_CTRL;
+	 writel_relaxed(temp, USB_USBCMD);
+
+	 /*
+	 * Add memory barrier as it is must to complete
+	 * above USB PHY and Link register writes before
+	 * moving ahead with USB peripheral mode enumeration,
+	 * otherwise USB peripheral mode may not work.
+	 */
+	 mb();
+#undef MSM_USB_BASE
+}
+#endif
+
 static int ci13xxx_pullup(struct usb_gadget *_gadget, int is_active)
 {
 	struct ci13xxx *udc = container_of(_gadget, struct ci13xxx, gadget);
 	unsigned long flags;
 
 	spin_lock_irqsave(udc->lock, flags);
+
+#if IS_ENABLED(CONFIG_MACH_XIAOMI_SDM439)
+	if (xiaomi_sdm439_mach_get()) {
+		pr_info("%s,softconnec=%d,is_active=%d\n", __func__, udc->softconnect, is_active);
+
+		if (!udc->softconnect && is_active) {
+			pr_info("%s %d\n", __func__, __LINE__);
+			xiaomi_sdm439_ci13xxx_udc_connect(udc);
+			mdelay(100);
+		}
+	}
+#endif
+
+
 	udc->softconnect = is_active;
 	if (((udc->udc_driver->flags & CI13XXX_PULLUP_ON_VBUS) &&
 			!udc->vbus_active) || !udc->driver) {
2be2425fa176c942e2e498750ac786fb977373d7
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-15 04:37:34 +0800

    fs: proc: Add option to append androidboot.force_normal_boot=1 to /proc/cmdline
    * For Android P launched A/B devices migrating to Android Q
      style System-as-root, This must be passed to cmdline when
      booting into normal or charger mode.
    * `androidboot.mode=recovery` may not be passed to cmdline
      when booting into recovery, but `skip_initramfs` is always
      present on cmdline when booting into system, so determine
      boot mode using `skip_initramfs` instead.
    
    Change-Id: Ia00ea2c54e2a7d2275e552837039033adb98d0ff

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -3,14 +3,13 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #include <asm/setup.h>
-#endif
 
 #ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 #define INITRAMFS_STR_FIND "skip_initramf"
 #define INITRAMFS_STR_REPLACE "want_initramf"
 #define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+#endif
 
 static char proc_command_line[COMMAND_LINE_SIZE];
 
@@ -19,30 +18,28 @@ static void proc_command_line_init(void) {
 
 	strcpy(proc_command_line, saved_command_line);
 
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
-
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
-}
+	if (offset_addr)
+		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
 #endif
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+	if (strstr(saved_command_line, "skip_initramfs"))
+		snprintf(proc_command_line, COMMAND_LINE_SIZE, "%s androidboot.force_normal_boot=1", proc_command_line);
+#endif
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	seq_puts(m, proc_command_line);
-#else
-	seq_puts(m, saved_command_line);
-#endif
 	seq_putc(m, '\n');
 	return 0;
 }
 
 static int __init proc_cmdline_init(void)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
 	proc_command_line_init();
-#endif
 
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
 	return 0;

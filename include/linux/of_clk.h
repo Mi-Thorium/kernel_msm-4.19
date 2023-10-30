620be9720d5d9da014ba7c1edbdc737b5dd0863d
Yumi Yukimura <me.cafebabe@gmail.com>
2023-10-30 16:22:58 +0800

    clk: msm: Add snapshot of clock framework files [2/2]
    * Resolve merge conflicts and fixup for msm-4.19
    * Do not change any behavior for common clk framework
    
    Change-Id: I5b08205eed3b9ecc058dcb69dbfa27c0a88bf2c9

--- include/linux/of_clk.h
+++ include/linux/of_clk.h
@@ -6,13 +6,17 @@
 #ifndef __LINUX_OF_CLK_H
 #define __LINUX_OF_CLK_H
 
-#if defined(CONFIG_COMMON_CLK) && defined(CONFIG_OF)
+#if defined(CONFIG_OF)
 
 unsigned int of_clk_get_parent_count(struct device_node *np);
 const char *of_clk_get_parent_name(struct device_node *np, int index);
+#ifdef CONFIG_COMMON_CLK
 void of_clk_init(const struct of_device_id *matches);
+#else
+static inline void of_clk_init(const struct of_device_id *matches) {}
+#endif
 
-#else /* !CONFIG_COMMON_CLK || !CONFIG_OF */
+#else /* !CONFIG_OF */
 
 static inline unsigned int of_clk_get_parent_count(struct device_node *np)
 {
@@ -25,6 +29,6 @@ static inline const char *of_clk_get_parent_name(struct device_node *np,
 }
 static inline void of_clk_init(const struct of_device_id *matches) {}
 
-#endif /* !CONFIG_COMMON_CLK || !CONFIG_OF */
+#endif /* !CONFIG_OF */
 
 #endif /* __LINUX_OF_CLK_H */

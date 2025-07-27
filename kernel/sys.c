45e8cbc08538d82e5b9340b01c9de53e5710f0a9
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-27 14:29:19 +0800

    syscall: Increase bpf fake uname to 5.10.199
    Change-Id: I16c853b434767c2730356b02bf70fe49685b48ad

--- kernel/sys.c
+++ kernel/sys.c
@@ -1249,7 +1249,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "5.4.186");
+		strcpy(tmp.release, "5.10.199");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}

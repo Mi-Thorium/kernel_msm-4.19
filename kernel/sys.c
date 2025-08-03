3000bc534ed3e0f6482cb380214a2c37aababdd5
Yumi Yukimura <me.cafebabe@gmail.com>
2025-08-03 22:37:20 +0800

    syscall: Spoof for uprobestats as well
    * As suggested by someone
    
    Change-Id: I39025f9e9b67fcfdf34defe7897be3e616a6a918

--- kernel/sys.c
+++ kernel/sys.c
@@ -1248,7 +1248,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
 		strcpy(tmp.release, "5.10.199");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
